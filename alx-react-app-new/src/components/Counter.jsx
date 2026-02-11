6fdc7b5e1484fda5d286c693b68dd08954b9ee15
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-11 19:30:37 +0100

    Feat: Styling Counter Buttons

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
 function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <div>
-            <p>{count}</p>
+        <div style={{ backgroundColor: 'lightblue', padding: '20px', borderRadius: '5px' }}>
+            <p style={{ fontSize: '36px', fontWeight: 'bold', color: 'darkblue' }}>{count}</p>
 
-            <div>
-                <button onClick={() => setCount(count - 1)}>Decrement</button>
-                <button onClick={() => setCount(count + 1)}>Increment</button>
-                <button onClick={() => setCount(0)}>Reset</button>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+                <button onClick={() => setCount(count - 1)} style={{backgroundColor: 'yellow', color: 'black' }}>Decrement</button>
+                <button onClick={() => setCount(count + 1)} style={{backgroundColor: 'green'}}>Increment</button>
+                <button onClick={() => setCount(0)} style={{backgroundColor: 'red' }}>Reset</button>
             </div>
         </div>
     );
