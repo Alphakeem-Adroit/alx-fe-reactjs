ee250c3d0fc9c6d83308b484800489563483e46f
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-11 21:38:11 +0100

    Fix: UserContext Proper Directory Location

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,9 +1,9 @@
 import { useContext } from "react";
-import UserContext from '../UserContext.js';
+import UserContext from '../../UserContext.js';
 
 function UserDetails() {
     const userData = useContext(UserContext);
-    
+
     return (
         <div>
             <p>Name: {userData.name}</p>
