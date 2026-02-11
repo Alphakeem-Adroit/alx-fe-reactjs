6c25945650c48fbd0d3160fc1c404540b00f3a07
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-11 18:33:51 +0100

    Feat: Applying Inline CSS Styling to React Components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+        <footer style={{ backgroundColor: 'lightgray', color: 'black', padding: '10px', textAlign: 'center', borderRadius: '5px' }}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
