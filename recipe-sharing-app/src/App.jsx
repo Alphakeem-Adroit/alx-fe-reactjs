51783ffd91d4f0fb689c34fc4d9a2265bead5054
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-14 18:16:33 +0100

    Feat: Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,10 @@
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+import DeleteRecipeButton from './components/DeleteRecipeButton';
+
 
 function App() {
   return (
@@ -11,3 +16,18 @@ function App() {
 }
 
 export default App
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<RecipeList />} />
+//         <Route path="/recipe/:id" element={<RecipeDetails />} />
+//         <Route path="/edit/:id" element={<EditRecipeForm />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,27 @@
 import { create } from 'zustand';
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  // Delete a recipe by id
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Update an existing recipe by id
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
 }));
 
 export default useRecipeStore;
