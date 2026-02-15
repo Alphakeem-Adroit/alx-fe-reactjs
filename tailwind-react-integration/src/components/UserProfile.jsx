3d336e18924a0fd0be2799b7d50aeec2a484a53c
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-15 20:46:11 +0100

    Fix: Responsive container adjustments + Responsive Image Sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ import react from '../assets/react.svg'
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-      <img src={ react } alt="User" className="rounded-full w-36 h-36 mx-auto" />
+    <div className="bg-gray-100 p-8 sm:p-4 md:p-8 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+      <img src={ react } alt="User" className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" />
       <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
       <p className="text-gray-600 sm:text-sm  md:text-base text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
