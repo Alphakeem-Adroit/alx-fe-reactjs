051482cc6a89bfa62b06dbbbdf3012055bf21241
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-19 17:53:32 +0100

    Chore: Import Food Images

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+      <HomePage />
+    </div>
   )
 }
 
